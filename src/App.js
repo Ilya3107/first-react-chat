52d4c786acdb238e1437d7e7f3813f2c41b9c013
”Ilya <poryadinilya3107@gmail.com>
2023-01-15 18:45:40 +0300

    remove to start

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+
 import "./App.css";
-import AppRouter from "./components/AppRouter";
-import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <AppRouter />
-    </BrowserRouter>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
